44920f9ff56c06c3bb0100c882305071d0eda92a
hackboxguy <albert.david@gmail.com>
2025-07-26 11:33:49 +0200

    added installation of libftdi1 libhidapi-libusb0 dependency

--- setup.sh
+++ setup.sh
@@ -78,10 +78,10 @@ log "Using config file: $CONFIG_FILE" force
 printf "Installing dependencies ................................ "
 if [ $VERBOSE -eq 1 ]; then
     DEBIAN_FRONTEND=noninteractive apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get install -y cmake libudev-dev nlohmann-json3-dev iperf3 libcurl4-openssl-dev avahi-daemon avahi-utils libraspberrypi-bin fbi mpv
+    DEBIAN_FRONTEND=noninteractive apt-get install -y cmake libudev-dev nlohmann-json3-dev iperf3 libcurl4-openssl-dev avahi-daemon avahi-utils libraspberrypi-bin fbi mpv libftdi1 libhidapi-libusb0
 else
     DEBIAN_FRONTEND=noninteractive apt-get update < /dev/null > /dev/null
-    DEBIAN_FRONTEND=noninteractive apt-get install -y -qq cmake libudev-dev nlohmann-json3-dev iperf3 libcurl4-openssl-dev avahi-daemon avahi-utils libraspberrypi-bin fbi mpv < /dev/null > /dev/null
+    DEBIAN_FRONTEND=noninteractive apt-get install -y -qq cmake libudev-dev nlohmann-json3-dev iperf3 libcurl4-openssl-dev avahi-daemon avahi-utils libraspberrypi-bin fbi mpv libftdi1 libhidapi-libusb0 < /dev/null > /dev/null
 fi
 test 0 -eq $? && echo "[OK]" || { echo "[FAIL]"; exit 1; }
 
